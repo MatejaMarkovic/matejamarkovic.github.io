e5ea7814f2951ca59e1d0b84087612815cb06dbe
Mateja Marković <75374455+MatteyaDev@users.noreply.github.com>
2022-04-15 23:58:49 +0200

    Updated

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Mateja Marković - Video Production & Development</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="icon" type="image/jpg" href="favicon.jpg">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ms+Madi&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+</head>
+<body style="">
+    <div class="container">       
+        <div>
+            <img class="title" src="logo.png">
+        </div><br><br>
+        <div class="responsiveMedia">
+            <iframe src="https://www.youtube.com/embed/zTX4zYU79ik" style="border-radius: 15px;margin-top: 5%;" width="50%" title="Showreel" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div><br><br><br>
+        <div class="row text-center">
+            <h1 class="hr" style="text-decoration: underline;text-underline-offset: 0.1em;">Socials</h1>
+            <a href="https://www.instagram.com/_matejamarkovic_/" class="fa fa-instagram icon" target="_blank"></a><a class="icon" href="https://www.instagram.com/_matejamarkovic_/" target="_blank"> @_matejamarkovic_</a><br><br>
+            <a href="https://twitter.com/ignMatteya" class="fa fa-twitter icon" target="_blank"></a><a class="icon" href="https://twitter.com/xMatteya" target="_blank"> @xMatteya</a><br><br>
+            <a href="https://github.com/MatteyaDev" class="fa fa-github icon" target="_blank"></a><a class="icon" href="https://github.com/MatteyaDev" target="_blank"> @matteyadev</a><br><br>
+        </div><br><br><br>
+        <footer class="page-footer font-small blue">
+        <div class="row text-center" id="bio">
+        <h1 class="hr" style="text-decoration: underline;text-underline-offset: 0.1em;">Timeline</h1>
+            <p style="font-family: 'Inconsolata', monospace;color: #fff;text-align:center;"><b>2004</b> Born in Belgrade, Serbia.</p>
+            <p style="font-family: 'Inconsolata', monospace;color: #fff;text-align:center;"><b>2015</b> Got into Design and Gaming.</p>
+            <p style="font-family: 'Inconsolata', monospace;color: #fff;text-align:center;"><b>2023</b> Will Finish Highschool.</p>
+            <p style="font-family: 'Inconsolata', monospace;color: #fff;text-align:center;"><b>2027</b> Will Graduate!</p>
+        </div><br>
+        <div class="row text-center">
+        <h1 class="hr" style="text-decoration: underline;text-underline-offset: 0.1em;">Contact</h1>
+            <a target="_blank" href="mailto:matejamarkovicets@gmail.com" class="row text-align"><model-viewer src="paperplane.glb" auto-rotate auto-rotate-delay="0" disable-zoom rotation-per-second="400%" >
+                <div class="poster" slot="poster"></div>
+                <div class="poster" slot="progress-bar"></div>
+            </model-viewer></a><br>
+            <p style="font-family: 'Inconsolata', monospace;color: #fff;text-align:center;"><b>Click on the paper airplane!</b></p>
+        </div><br><br>
+        <div class="footer-copyright text-center py-3" style="color: #cecece;">
+        Copyright © 2022 Mateja Marković | Video Prod & Development
+        </div>
+        </footer>
+    </div>
+</body>
+</html>
